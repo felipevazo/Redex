60cdf209d68e72234cc5689ed08acbb850372586
felipevazo <f.vasquez07@ufromail.cl>
2017-05-11 18:23:27 -0300

    Update cliente.py

--- materia2/cliente.py
+++ materia2/cliente.py
@@ -1,1 +1,10 @@
-
+import socket()
+s=socket.socket()
+s.connect(('127.0.0.1',9999))
+while True:
+    msg = raw_input('>>> ') # metodo que lee hasta que el usuario apreta enter
+    s.send(msg)
+    if msg == "quit":
+        break
+print "shao loh vimoh"
+s.close()    
